2d07d8cb228177dea11e686021638f4c5df40da2
aminya <aminyahyaabadi74@gmail.com>
2020-01-24 18:10:11 -0600

    m2mlx 'all_exclude'

--- m2mlx.m
+++ m2mlx.m
@@ -3,3 +3,42 @@
         % To include all
         case 'all'
             mFiles=dir('**/*.m');
+
+        case 'all_exclude'
+            mFiles=dir('**/*.m');
+
+            % folders with their subfolders to be excluded
+            excludedFoldersWSub = varargin{1};
+
+            if ~isempty(excludedFoldersWSub)
+                % 1
+                mFiles = dir(fullfile([excludedFoldersWSub(i),"**/*.m"]));
+                % 2:end
+                for i=2:length(excludedFoldersWSub)
+                    mFiles = [mFiles; dir(fullfile([excludedFoldersWSub(i),"**/*.m"]))];
+                end
+            end
+
+            % folders without their subfolders to be excluded
+            if nargin == 2
+                excludedFoldersWOSub = varargin{2};
+
+                if ~isempty(excludedFoldersWOSub)
+                    for i=1:length(excludedFoldersWOSub)
+                        mFiles = [mFiles; dir(fullfile([excludedFoldersWOSub(i),"*.m"]))];
+                    end
+                end
+            end
+
+            % excluding files of that folder
+            removeList=zeros(size(mFilesExclude));
+            iRem=1;
+            for i=1:size(mFiles,1)
+                if  ismember({mFiles(i).folder},{mFilesExclude(:).folder})
+                    removeList(iRem)=i;
+                    iRem=iRem+1;
+                end
+            end
+
+            mFiles(removeList)=[];
+
